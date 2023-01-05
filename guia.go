f484446dac9509c778d07b8e4e48ed743de394b0
Gabriel <caceresgabriel2001@gmail.com>
2023-01-05 17:36:11 -0500

    segundo commit

--- guia.go
+++ guia.go
@@ -8,7 +8,17 @@ git init    - inicia el repositorio en la carpeta seleccionada
             crea 2 carpetas invisibles en dicho repositorio
 git status  - para ver los archivos que se tienen
             rojo significa que esta en el aire, no se agrego al entorno de trabajo
-git add ""  - Agregas el archivo a tu entorno de trabajo
+git add ""  - Agregas el archivo a tu entorno de trabajo. (git add guia.go)
+            aparecerá en azul pues ya se incluyó
+
+git commit  - crea un punto de control del código, pues puede tener cambios
+        así que primero se crea unarchivo para configurar
+
+git config --global user.email "direccion@correo.com"   -si se escribe mal solo se vuelve a poner, ya que se sobreescribe
+git config --global user.name "Gabriel"    - sin comillas se ponen ambos
+
+git commit  - abre el editor de código, si no deja editar hay que presionar la tecla 'I'
+            cuando se termine se presiona "Esc" y se pone ":wq"
 
 
 */
