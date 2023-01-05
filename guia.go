7de15ff854f8278ecfe2c61feccac8d76afa453b
Gabriel <caceresgabriel2001@gmail.com>
2023-01-05 17:31:37 -0500

    primer commit

---
+++ guia.go
@@ -0,0 +1,14 @@
+package guia
+
+/*
+pwd         - para saber donde se esta
+cd "        - para navegar (..)"arriba" (name)"escritorio al cual se va"
+ls          - para ver lo que contiene la carpeta
+git init    - inicia el repositorio en la carpeta seleccionada
+            crea 2 carpetas invisibles en dicho repositorio
+git status  - para ver los archivos que se tienen
+            rojo significa que esta en el aire, no se agrego al entorno de trabajo
+git add ""  - Agregas el archivo a tu entorno de trabajo
+
+
+*/
